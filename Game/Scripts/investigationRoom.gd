e18ab7dde7e0aa5c422e208370582e1f28608905
Fábio "DannyIsYog" Sousa <fabio.sousa@tecnico.ulisboa.pt>
2023-12-08 20:05:23 +0000

    added json

---
+++ Game/Scripts/investigationRoom.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var json_file_path : String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func read_json():
+	var file = FileAccess.open(json_file_path, FileAccess.READ)
+	var content = file.get_as_text()
+	var json = JSON.new()
+	var finish = json.parse_string(content)
+	return finish
